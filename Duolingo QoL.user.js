96ad234105fa9747dce52a8e148401abd0f6310a
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2022-03-03 10:06:20 +0200

    Update Duolingo QoL.user.js

--- Duolingo QoL.user.js
+++ Duolingo QoL.user.js
@@ -15,35 +15,63 @@ const storiesToHide = [
 	"The Honeymoon",
 	"A Very Big Family",
 	"Where Is Your Girlfriend?",
+	"A Love Letter",
+	"The Ex-Girlfriend",
+	"Why Did She Break Up with Me?",
+	"I'm Going to Rome",
+	"Do You Want to Break up with Me?",
+	"Is It Love?"
+]
+
+const imagesToHide = [
+  "https://d2pur3iezf4d1j.cloudfront.net/images/af72ee551cceae45eccbe363900213d3",
+  "https://d2pur3iezf4d1j.cloudfront.net/images/204f805969b1a0e77cfdf93494a81998",
+  "https://d2pur3iezf4d1j.cloudfront.net/images/64daad7ae9d55baae9a97ab65a1fc944",
+  "https://d2pur3iezf4d1j.cloudfront.net/images/2e8958904b95485dc7d6c9d4f412f697",
+  "https://d2pur3iezf4d1j.cloudfront.net/images/ac5c08e95c0fdcc4bba2502616658e00",
 ]
 
 /* == Helper functions == */
 
-const qs = selector => document.querySelector(selector)
+const $ = selector => document.querySelectorAll(selector)
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-// `console` is overwritten by Duolingo.com. So we need to steal the original `console` from an iframe.
+// `console` is overwritten by Duolingo, So we need to steal the original `console` from an iframe.
 // Credit: https://stackoverflow.com/a/7081490
 const consoleFrame = document.createElement('iframe')
 consoleFrame.style.display = "none"
 document.body.appendChild(consoleFrame)
-const log = consoleFrame.contentWindow.console.log
+const console = consoleFrame.contentWindow.console
 
 /* == QoL Functions == */
 
-// Scrolls to the first uncompleted skill:
+/** Scrolls to the first uncompleted skill */
 const scrollLearn = async () => {
-	// Find the first uncompleted skill (based on class):
-	let skill = null
-	while (!skill) {
-		await delay(100)
-		skill = Array.from(document.querySelectorAll("[data-test='skill-icon']"))
-			.filter(el => !Array.from(el.children[0].classList).includes("_3dqWQ"))[0]
-	}
+  await delay(100) // Give the page some time to load
 
-	// Scroll to skill:
-	skill.scrollIntoView()
-	scrollBy(0, -450)
+  while (true) {
+    const allSkills = Array.from($("[data-test='skill']"))
+
+    if (allSkills.length > 0) { // Don't run before the page finished loading
+      const loadedSkills = Array.from($("[data-test='skill-icon']"))
+      const uncompletedSkills = loadedSkills.filter(el => !Array.from(el.children[0].classList).includes("_3dqWQ"))
+
+      // Scroll until we get to the first uncompleted skill
+      if (uncompletedSkills.length === 0 && loadedSkills.length < allSkills.length) {
+        loadedSkills.at(-1).scrollIntoView() // Scroll to the last loaded skill
+        await delay() // Wait for more skills to load
+      } else {
+        const targetSkill = uncompletedSkills[0] // Find the first uncompleted skill
+
+        // Scroll to skill:
+	      targetSkill.scrollIntoView()
+        scrollBy(0, -450)
+        break
+      }
+    } else { // Give the page some time to load
+      await delay(50)
+    }
+	}
 }
 
 // Cleans Duolingo Stories from stupid stories:
@@ -54,7 +82,7 @@ const cleanStories = async () => {
 	// If there are no stories at all, wait for the page to load and try again:
 	while (stories.length === 0) {
 		await delay(100)
-		stories = Array.from(document.querySelectorAll("._1eZU8"))
+		stories = Array.from($("._1eZU8"))
 	}
 
 	// Remove stories:
@@ -69,7 +97,7 @@ const scrollStories = async () => {
 	let story = null
 	while (!story) {
 		await delay(100)
-		story = Array.from(document.querySelectorAll("._2zY7s"))
+		story = Array.from($("._2zY7s"))
 			.filter(el => !["rgb(251, 177, 0)", "rgb(255, 177, 0)"].includes(el.style.backgroundColor))[0]
 	}
 
@@ -95,7 +123,13 @@ const urlTracker = new MutationObserver(async () => {
 	}
 })
 
+/* == Custom CSS == */
+
+const style = document.createElement('style')
+style.innerHTML = 'img[src="' + imagesToHide.join('"], img[src="') + ' { width: 0px !important }'
+
 /* == Initialization == */
 
+document.body.append(style)
 urlTracker.cache = {}
-urlTracker.observe(document, {attributes: true, subtree: true})
+urlTracker.observe(document, { childList: true, subtree: true })
