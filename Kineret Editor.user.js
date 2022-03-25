43aac048847ea86af26a7617a482d3ac4d16b42a
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2022-03-25 16:20:17 +0300

    Update Kineret Editor.user.js

--- Kineret Editor.user.js
+++ Kineret Editor.user.js
@@ -3,7 +3,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Michael T
-// @match        http://kineret.org.il/
+// @match        https://kineret.org.il/
 // @grant        none
 // ==/UserScript==
 
@@ -19,9 +19,9 @@
         $(element).text(miflas)
         element.innerHTML += `<br><a id="wikipedia" href="//he.wikipedia.org/w/index.php?title=הכנרת&action=edit" target="_blank">הכנרת - ויקיפדיה</a>`
         $("#wikipedia").css({"color":"#6800ff42"})
-        $(element).click( () => {
-             //currently does not work since the page is not https
-            //navigator.clipboard.writeText(miflas)
+        $(element).click(async () => {
+            // Finally works since the page is now https
+            await navigator.clipboard.writeText(miflas)
             console.log("clicked")
         })
     }, 2000)
